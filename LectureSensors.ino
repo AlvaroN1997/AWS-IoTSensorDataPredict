56ffd72c2376587b8ef4c5557debd54b9a6826f6
AlvaroN1997 <38118994+AlvaroN1997@users.noreply.github.com>
2019-10-05 19:19:00 -0500

    Update LectureSensors.ino
    Changed language

--- LectureSensors.ino
+++ LectureSensors.ino
@@ -2,60 +2,53 @@
 #include <DHT_U.h>
 
 int sensor = 2;
-int temperatura;
-int humedad;
+int temp;
+int humidity;
 
-const int MQ2sensor   = A0;         // Definir la entrada analógica de MQ2
-const int MQ5sensor   = A1;         // Definir la entrada analógica de MQ5
-const int MQ7sensor   = A2;         // Definir la entrada analógica de MQ7
-const int MQ135sensor = A3;       // Definir la entrada analógica de MQ135
-const float RL = 1.0;             // El valor RL es la resistencia que tienen los modulos MQ
-const float aire[4] = {9.83, 6.50, 27.10, 3.65};   // Valores aproximados del datasheet d los sensores MQ
-float RS;                         // kohm
-float RO;                         // kohm
-float ratio;                       // RS/RO
-int valor_crudo;                  // 0 a 1023
+const int MQ2   = A0;                             // Define analogic entrance of MQ2
+const int MQ5   = A1;                             // Define analogic entrance of MQ5
+const int MQ7   = A2;                             // Define analogic entrance of MQ7
+const int MQ135 = A3;                             // Define analogic entrance of MQ135
+const float RL = 1.0;                             // We're using MQ Modules, this modules has their own resistances
+const float air[4] = {9.83, 6.50, 27.10, 3.65};   // Aproximated values of air in MQs datasheets
+float RS;                                         // kohm
+float RO;                                         // kohm
+float ratio;                                      // RS/RO
+int raw_value;                                    // Values of analogic entrance between 0 to 1023
+DHT dht(sensor,DHT22);
 
-// Calibración del sensor
-int muestra_calibracion            = 100;
-int intervalo_calibracion          = 500;
-int lectura_muestras               = 50;
-int lectura_intervalos             = 5;
-
-// Se definen 3 variables para que se detecte los 3 valores, los cuales son GLP, CO y HUMO
-#define   HUMO       0
-#define   GLP        1
+// We define 4 variables to detect their values, they´re smoke, LPG, CO and NH4
+#define   SMOKE      0
+#define   LPG        1
 #define   CO         2
 #define   NH4        3
 
-// COMO SE HACE LA CALIBRACION
-// Las 3 curvas de cada gas pueden ser vistas en el datasheet del mq2, se toman los puntos, se saca la pendiente
-// Cada uno muestra valores de PPM distintos de acuerdo a la sensibilidad del sensor, entonces este capta de una forma distinta
-// cada uno de los componentes que se encuentren en el aire. En este caso para cada uno, se toma el valor que se muestra en el
-// datasheet. Ahora para conseguir los 3 valores importante de cada concentración de gas, se deben de tener 3 valores importantes
-// estos valores son X, Y y la pendiente, los cuales servirán para la calibración del dispositivo. NOTA: Los datos que se muestran
-// en el datasheet se tiene que sacar su logaritmo (en base 10), para obtener los valores reales de la gráfica. ¿Por que se usa
-// logaritmo? Porque el creador uso los logaritmos para que el grafico se vea lineal, sino se vería curvo, lo cual seria más
-// dificil sacar los datos del gráfico. Se debe de usar la formula de la linea, la cual es "Y = mX + B"
-// Utilizaremos el MQ2 como ejemplo, se van a tomar 2 puntos del grafico, donde tomaremos los siguiente puntos para GLP
-// (200,1.6) y (10000,0.26). Ahora a estos valores se tiene que sacar su logaritmo de la siguiente forma
-// (log(200), log(1.6)) y (log(10000), log(0.26)), y con los valores obtenidos de los logaritmos, se procede a hallar la pendeinte
-// de esos dos puntos, de la siguiente forma " m = [log(y) - log(y0)] / [log(x) - log(x0)]", y aplicando los puntos anteriores se
-// veria de la siguiente manera " m = [log(0.26) - log(1.6)] / [log(10000) - log(200)]" el cual da de resultado -0.46448 (-0.47).
-// Luego tenemos que hallar el valor "b" de la función lineal, en este caso tomamos el punto (5000,0.35) para hallar b, y se resuelve
-// de la siguiente manera: "log(0.36) = -0.47 * log(5000) + b", donde el valor de b es de 1.27
+/* How we calibrate?
+ To calibrate the sensors, first we need to see the datasheet of the sensor, then you can see a graph which has different components (CO, Amoniac, etc)
+ The graph has 2 values: Rs/Ro and PPM. So we're goint to calculate the component we want to choose, in this case que use smoke for the MQ2.
+ Now to get the 3 most important values for the concentration of a gas, we need to detect the X, Y and slope values, which will be important for the calibration
+ For example, in the MQ2 datasheet, to get a precise value, we take 5 points of the graph in a excel archive so we can calculate the slope, then after that, we 
+ need to calcule the value of the line or the straight ("Y = mX + B"). 
+ NOTE: THE GRAPH IS BASED IN A LOGARITMIC FUNCTION WITH BASE 10
+ Here is an example to calculate the slope, the line and using the logaritmic function, we're goint to calcule the smoke ppm values for mq2
+ Two points for the slope and its calculation
+ (200,1.6) and (10000,0.26) --- transforming it to log(10) ----> (log(200), log(1.6)) y (log(10000), log(0.26)) 
+ We apply "m = [log(y) - log(y0)] / [log(x) - log(x0)]", getting -0.44340257 (-0.44). NOTE: IT DEPEDNS HOW YOU TAKE THE POINT OF THE GRAPH
+ Then we calculate "b" value of the line, so que take the point (5000,0.91) to calculate "b", to do the next equation
+"log(0.36) = -0.47 * log(5000) + b" where "b" value is 1.5991
+ */
 
-// Puntos de cada curva. Cada sensor con su respectivo componente
-float curvaHUMO[3]  = {2.30, 0.53, -0.44}; 
-float curvaGLP[3]   = {2.30, -0.15, -0.37};
-float curvaCO[3]    = {1.70, 0.24, -0.677};
-float curvaNH4[3]   = {1.00, 0.46, -0.235};
+// Point on every curve, each sensor has their own calibration in their datasheet
+float SMOKEcurve[3]  = {2.30, 0.53, -0.44}; 
+float LPGcurve[3]    = {2.30, -0.15, -0.37};
+float COcurve[3]     = {1.70, 0.24, -0.677};
+float NH4curve[3]    = {1.00, 0.46, -0.235};
 
-float ROhumo = 10;
-float ROglp = 10;
-float ROco = 10;
-float ROnh4 = 10;
-// IMPORTANTE, PRIMERO SE TIENE QUE HALLAR EL VALOR R0, AL CALIBRAR LOS SENSORES, YA QUE ESTE VA A TENER UN VALOR FIJO
+float ROsmoke = 10;
+float ROlpg   = 10;
+float ROco    = 10;
+float ROnh4   = 10;
+// NOTE: first we need to find R0 value to calibrate the sensors because this values needs to be constant
 
 void setup() {
   
@@ -63,19 +56,19 @@ void setup() {
  dht.begin();
  pinMode(13,OUTPUT);
  digitalWrite(13,HIGH);
- Serial.print("Calibrando...");
+ Serial.print("Calibrating...");
 
- ROhumo = calibracionSensor(MQ2sensor, aire[0]);
- ROglp  = calibracionSensor(MQ5sensor, aire[1]);
- ROco   = calibracionSensor(MQ7sensor, aire[2]);
- ROnh4  = calibracionSensor(MQ135sensor, aire[3]);
+ ROsmoke = calibratingSensor(MQ2, air[0]);
+ ROlpg   = calibratingSensor(MQ5, air[1]);
+ ROco    = calibratingSensor(MQ7, air[2]);
+ ROnh4   = calibratingSensor(MQ135, air[3]);
  digitalWrite(13,LOW);
 
- //Serial.println("Calibracion terminada!");
- //Serial.print("RO-humo = ");
- //Serial.print(ROhumo);
- //Serial.print("RO-glp = ");
- //Serial.print(ROglp);
+ //Serial.println("Calibration finished!");
+ //Serial.print("RO-SMOKE = ");
+ //Serial.print(ROsmoke);
+ //Serial.print("RO-lpg = ");
+ //Serial.print(ROlpg);
  //Serial.print("RO-co = ");
  //Serial.print(ROco);
  //Serial.print("RO-nh4 = ");
@@ -84,26 +77,26 @@ void setup() {
 }
 
 void loop() {
-  long ppm_humo    = 0;
-  long ppm_glp     = 0;
+  long ppm_smoke   = 0;
+  long ppm_lpg     = 0;
   long ppm_co      = 0;
   long ppm_nh4     = 0;
   
-  ppm_humo   = obtener_porc_gas(lecturaMQ(MQ2sensor)/ROhumo, HUMO);
-  ppm_glp    = obtener_porc_gas(lecturaMQ(MQ2sensor)/ROglp, GLP);
-  ppm_co     = obtener_porc_gas(lecturaMQ(MQ2sensor)/ROco, CO);
-  ppm_nh4    = obtener_porc_gas(lecturaMQ(MQ135sensor)/ROnh4, NH4);
+  ppm_smoke   = obtain_gas_perc(MQlecture(MQ2)/ROsmoke, SMOKE);
+  ppm_lpg     = obtain_gas_perc(MQlecture(MQ5)/ROlpg, LPG);
+  ppm_co      = obtain_gas_perc(MQlecture(MQ7)/ROco, CO);
+  ppm_nh4     = obtain_gas_perc(MQlecture(MQ135)/ROnh4, NH4);
 
-  temperatura = dht.readTemperature();
-  humedad = dht.readHumidity();
+  temp = dht.readTemperature();
+  humidity = dht.readHumidity();
 
-  Serial.println("**********         VALORES PPM          **********");
-  Serial.print("HUMO: ");
-  Serial.print(ppm_humo);
+  Serial.println("**********         PPM VALUES          **********");
+  Serial.print("SMOKE: ");
+  Serial.print(ppm_smoke);
   Serial.println(" ppm");
   
-  Serial.print("GLP: ");
-  Serial.print(ppm_glp);
+  Serial.print("LPG: ");
+  Serial.print(ppm_lpg);
   Serial.println(" ppm");
   
   Serial.print("CO: ");
@@ -114,67 +107,65 @@ void loop() {
   Serial.print(ppm_nh4);
   Serial.println(" ppm");
 
-  Serial.print("Temperatura: ");
-  Serial.print(temperatura);
+  Serial.print("Temperature: ");
+  Serial.print(temp);
   Serial.print("°C");
 
-  Serial.print("Humedad: ");
-  Serial.print(ppm_nh4);
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
   Serial.println("%");
-  delay(500);
+  delay(1000);
 }
 
-//  FUNCIONES
-// Permite conseguir el valor RS de acuerdo lo que recibe el sensor
-float calcularRS(int crudo){
-  return (((float) RL * (1023 - crudo) / crudo));
+// FUNCTIONS
+// Get RS value
+float calculateRS(int raw){
+  return (((float) RL * (1023 - raw) / raw));
 }
 
-// Permite hacer la calibracion del sensor definiendo el pin de conexion analogico
-// La funcion permite calcular el Ro
-float calibracionSensor(int pin, float aireSensor){
+// Get R0 value
+float calibratingSensor(int pin, float sensorAir){
   int i;
   float val=0;
               
-  for (i=0; i<muestra_calibracion;i++){
-    val += calcularRS(analogRead(pin));
-    delay(intervalo_calibracion);
+  for (i=0; i<100; i++){
+    val += calculateRS(analogRead(pin));
+    delay(50);
   }
-  val = val/muestra_calibracion;
-  val = val/aireSensor;
+  val = val/100;
+  val = val/sensorAir;
   return val;
 }
 
-// Esta funcion sirve para volver a calcular el RS del sensor, esto se debe a que el RS cambia de acuerdo a las distintas
-// concentraciones que hay en el datasheet.
-float lecturaMQ(int pin){
+// This function is for calculating the average RS of the first 50 RS values
+float MQlecture(int pin){
   int i;
   float rs=0;
 
-  for (i=0; i<lectura_intervalos;i++){
-    rs += calcularRS(analogRead(pin));
-    delay(lectura_muestras);
+  for (i=0; i<50; i++){
+    rs += calculateRS(analogRead(pin));
+    delay(50);
   }
-  rs = rs/lectura_intervalos;
+  rs = rs/50;
   return rs;
 }
 
-// Esta funcion determina las curvas que se van a detectar, es mas un ordenador, para definir lo que va a hacer el sensor y el 
-// componente que se quiere leer.
-long obtener_porc_gas(float ratio_rs_ro, int id_gas){
-  if (id_gas == HUMO){
-    return obtener_porc_sensor(ratio_rs_ro,curvaHUMO);
-  } else if (id_gas == GLP){
-    return obtener_porc_sensor(ratio_rs_ro,curvaGLP);
+// This function determines which curves we want to be detected, so we define what the sensor will do and the component that will be read
+long obtain_gas_perc(float ratio_rs_ro, int id_gas){
+  if (id_gas == SMOKE){
+    return obtain_perc_sensor(ratio_rs_ro,SMOKEcurve);
+  } else if (id_gas == LPG){
+    return obtain_perc_sensor(ratio_rs_ro,LPGcurve);
   } else if (id_gas == CO){
-    return obtener_porc_sensor(ratio_rs_ro,curvaCO);
+    return obtain_perc_sensor(ratio_rs_ro,COcurve);
   } else if (id_gas == NH4){
-    return obtener_porc_sensor(ratio_rs_ro,curvaNH4);
+    return obtain_perc_sensor(ratio_rs_ro,NH4curve);
   }
   return 0;
 }
 
-long obtener_porc_sensor(float ratio_rs_ro, float *pcurve){
+// This function allows to get the PPM values according the data sheet of each MQ sensor
+long obtain_perc_sensor(float ratio_rs_ro, float *pcurve){
   
   return (pow(10,( ((log(ratio_rs_ro)-pcurve[1])/pcurve[2]) + pcurve[0])));
 }
